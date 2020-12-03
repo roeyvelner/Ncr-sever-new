0ce3b3b7c57272be94f6b654c891e0cd91d3c3af
Velner <rv250161@ncr.com>
2020-12-03 18:42:03 +0200

    chagne about

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', (req,res)=>{
     res.send("Welcome");
 });
 
+app.get('/about', (req,res)=>{
+    res.send("about");
+});
+
 const port = process.env.port || 3000;
 app.listen(port, ()=>{
     console.log("Hello!!")

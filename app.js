de38707553fb58adee05841cf1c34f80525426ee
Velner <rv250161@ncr.com>
2020-12-22 12:01:21 +0200

    add http server as express and webSocketServer

--- app.js
+++ app.js
@@ -17,91 +17,117 @@
 //     console.log("Hello!!")
 // });
 
+var WebSocketServer = require('websocket').server;
+var express         = require('express');
+var app             = express();
+var server          = app.listen(8080);
+var wsServer        = new WebSocketServer({ httpServer : server });
 
+// this will make Express serve your static files
+app.use(express.static(__dirname + '/public'));
 
-
-//////////////////// webSocket Try  /////////////////////////////////////
-const WebSocket = require('ws')
-var http = require('http');
-var url = require("url");
-
-
-console.log("before open webSocket");
-
-const wss = new WebSocket.Server({ port: 8080 })
-
-console.log("after open webSocket");
-
-wss.on('connection', ws => {
-
-    console.log("has connection ");
-    http.createServer(function (req, res) {
-        console.log("got request " + i);
-        i++;
-    
-        var parsedUrl = url.parse(req.url, true); // true to get query as object
-      var queryAsObject = parsedUrl.query;
-    
-       console.log(JSON.stringify(queryAsObject));
-    //   console.log("party= == " + queryAsObject.party);
-        // res.writeHead(200, {'Content-Type': 'application/json','Access-Control-Allow-Origin': 'http://localhost:3000'});
-        res.writeHead(200, {'Content-Type': 'application/json','Access-Control-Allow-Origin': 'http://ncr-raanana.s3-website.us-east-2.amazonaws.com'});
-        if (!isEmpty(queryAsObject)){
-            var isTrueSet = (queryAsObject.party === 'true');
-            flag =isTrueSet;
+// the rest of your code
+wsServer.on('request', function(r) {
+    var connection = request.accept('echo-protocol', request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            connection.sendUTF(message.utf8Data);
         }
-        if (isTrueSet)
-            ws.send("תחילת מכירה");
-        else
-            ws.send("סיום מכירה");
-        res.end(JSON.stringify({ ans: flag }));
-    }).listen(8083);
+        else if (message.type === 'binary') {
+            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            connection.sendBytes(message.binaryData);
+        }
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+}
 
-    console.log("start http server on 8080");
+
+// //////////////////// webSocket  first   Try  /////////////////////////////////////
+// const WebSocket = require('ws')
+// var http = require('http');
+// var url = require("url");
+
+
+// console.log("before open webSocket");
+
+// const wss = new WebSocket.Server({ port: 8080 })
+
+// console.log("after open webSocket");
+
+// wss.on('connection', ws => {
+
+//     console.log("has connection ");
+//     http.createServer(function (req, res) {
+//         console.log("got request " + i);
+//         i++;
+    
+//         var parsedUrl = url.parse(req.url, true); // true to get query as object
+//       var queryAsObject = parsedUrl.query;
+    
+//        console.log(JSON.stringify(queryAsObject));
+//     //   console.log("party= == " + queryAsObject.party);
+//         // res.writeHead(200, {'Content-Type': 'application/json','Access-Control-Allow-Origin': 'http://localhost:3000'});
+//         res.writeHead(200, {'Content-Type': 'application/json','Access-Control-Allow-Origin': 'http://ncr-raanana.s3-website.us-east-2.amazonaws.com'});
+//         if (!isEmpty(queryAsObject)){
+//             var isTrueSet = (queryAsObject.party === 'true');
+//             flag =isTrueSet;
+//         }
+//         if (isTrueSet)
+//             ws.send("תחילת מכירה");
+//         else
+//             ws.send("סיום מכירה");
+//         res.end(JSON.stringify({ ans: flag }));
+//     }).listen(8083);
+
+//     console.log("start http server on 8080");
 
 
 
-//   ws.on('message', message => {
-//     console.log(`Received message => ${message}`)
+// //   ws.on('message', message => {
+// //     console.log(`Received message => ${message}`)
 
-//     if (message === 'Start Sale'){
+// //     if (message === 'Start Sale'){
 
-//     }
-//   })
+// //     }
+// //   })
 
-/**
-//   setInterval(()=>{
-//     var num = Math.floor(Math.random() * 10);  
-//     switch (num){
-//         case 1:
-//             ws.send('שלום אלי');
-//             break;
-//         case 2:
-//             ws.send('שלום רועי');
-//             break;
+// /**
+// //   setInterval(()=>{
+// //     var num = Math.floor(Math.random() * 10);  
+// //     switch (num){
+// //         case 1:
+// //             ws.send('שלום אלי');
+// //             break;
+// //         case 2:
+// //             ws.send('שלום רועי');
+// //             break;
             
-//         case 3:
-//             ws.send('שלום בובה');
-//             break;
-//         case 4:
-//             ws.send('שלום משה');
-//             break;
+// //         case 3:
+// //             ws.send('שלום בובה');
+// //             break;
+// //         case 4:
+// //             ws.send('שלום משה');
+// //             break;
 
-//         case 5:
-//             ws.send('שלום איתי');
-//             break;
-//         case 6:
-//             ws.send('שלום אסף');
-//             break;
-//         default:
-//     }
-//   },10000)
+// //         case 5:
+// //             ws.send('שלום איתי');
+// //             break;
+// //         case 6:
+// //             ws.send('שלום אסף');
+// //             break;
+// //         default:
+// //     }
+// //   },10000)
 
 
-  ws.send('ho!')
+//   ws.send('ho!')
 
-*/
-})
+// */
+// })
 
 
 

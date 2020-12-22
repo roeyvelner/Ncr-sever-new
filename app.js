cc3f8f940f9014ce4e59490e2b5822d5603c4eb7
Velner <rv250161@ncr.com>
2020-12-22 10:47:48 +0200

    add http server

--- app.js
+++ app.js
@@ -21,16 +21,49 @@
 
 
 //////////////////// webSocket Try  /////////////////////////////////////
+var http = require('http');
+var url = require("url");
 const WebSocket = require('ws')
- 
-const wss = new WebSocket.Server({ port: 8080 })
+
+const wss = new WebSocket.Server({ port: 8083 })
 
 wss.on('connection', ws => {
+
+
+    http.createServer(function (req, res) {
+        console.log("got request " + i);
+        i++;
+    
+        var parsedUrl = url.parse(req.url, true); // true to get query as object
+      var queryAsObject = parsedUrl.query;
+    
+       console.log(JSON.stringify(queryAsObject));
+    //   console.log("party= == " + queryAsObject.party);
+        // res.writeHead(200, {'Content-Type': 'application/json','Access-Control-Allow-Origin': 'http://localhost:3000'});
+        res.writeHead(200, {'Content-Type': 'application/json','Access-Control-Allow-Origin': 'http://ncr-raanana.s3-website.us-east-2.amazonaws.com'});
+        if (!isEmpty(queryAsObject)){
+            var isTrueSet = (queryAsObject.party === 'true');
+            flag =isTrueSet;
+        }
+        if (isTrueSet)
+            ws.send("תחילת מכירה");
+        else
+            ws.send("סיום מכירה");
+        res.end(JSON.stringify({ ans: flag }));
+    }).listen(8080);
+
+
+
+
   ws.on('message', message => {
     console.log(`Received message => ${message}`)
+
+    if (message === 'Start Sale'){
+
+    }
   })
 
-  
+/**
 //   setInterval(()=>{
 //     var num = Math.floor(Math.random() * 10);  
 //     switch (num){
@@ -60,13 +93,15 @@ wss.on('connection', ws => {
 
 
   ws.send('ho!')
+
+*/
 })
 
 
 
 
 
-//////////////////// new one  /////////////////////////////////////
+// //////////////////// new one  /////////////////////////////////////
 // var http = require('http');
 // var url = require("url");
 
@@ -95,6 +130,6 @@ wss.on('connection', ws => {
 // console.log("server is listening");
 
 
-// function isEmpty(obj) {
-//     return Object.keys(obj).length === 0;
-// }
+function isEmpty(obj) {
+    return Object.keys(obj).length === 0;
+}
